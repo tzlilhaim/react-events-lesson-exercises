6d14997aeaddfbe048fa69244fb1d834e3da4891
Hunter Weinsheink <hunteraweinsheink@gmail.com>
2019-04-23 18:00:37 +0300

    formatting

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import SpotCheck1 from './components/SpotCheck1';
 import SpotCheck2 from './components/SpotCheck2';
 import SpotCheck3 from './components/Spotcheck3';
-import SpotCheck5 from './components/Spotcheck5/SpotCheck5';
+import SpotCheck5 from './components/SpotCheck5/SpotCheck5';
 import Exercise1 from './components/Exercise1';
 import Exercise2 from './components/Exercise2/Exercise2';
+import SpotCheck4 from './components/SpotCheck4/SpotCheck4';
 
 class App extends Component {
   constructor() {
@@ -13,17 +14,34 @@ class App extends Component {
     this.state = {
     }
   }
-  
+
   render() {
-    const state = this.state
     return (
       <div >
-        <SpotCheck1/>
-        <SpotCheck2/>
-        <SpotCheck3/>
-        <SpotCheck5/>
-        <Exercise1/>
-        <Exercise2/>
+        <div class='ex-space'>
+          <SpotCheck1 />
+        </div>
+        <div class='ex-space'>
+          <SpotCheck2 />
+        </div>
+        <div class='ex-space'>
+          <SpotCheck3 />
+        </div>
+        <div class='ex-space'>
+          <SpotCheck4 />
+        </div>
+        <div class='ex-space'>
+          <SpotCheck5 />
+        </div>
+        <div class='ex-space'>
+        <Exercise1 />
+        </div>
+
+        <div class='ex-space'>
+
+        <Exercise2 />
+        </div>
+
       </div>
     );
   }
