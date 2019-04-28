e9266b9a7d979583470510d9521f77bb6e537450
Hunter Weinsheink <hunteraweinsheink@gmail.com>
2019-04-28 12:25:37 +0300

    added test to sc 3

--- tests/spot-check-3/sc-3.test.js
+++ tests/spot-check-3/sc-3.test.js
@@ -22,6 +22,11 @@ describe("spotcheck3", () => {
         expect(updatedState, `The likeQuote method should add 1 tp the likes of the first quote in your state's quotes property. 
                             After clicking once, the likes should be ${initialState+1} likes, you had ${updatedState} likes`)
             .toBe(initialState+1)
+        let renderedState = parseInt(wrapper.find('sup').first().text())
+        expect(renderedState, 
+          `You should change the state with the setState function so the app automatically renders when changes are made.
+            Expected the first quote to render ${updatedState} likes, instead rendered ${renderedState} likes`)
+          .toBe(updatedState)
       });
     it('Your likeQuote method should not affect anything other than the likes of the first quote', () => {
         const wrapper = mount(<SpotCheck3/>)
