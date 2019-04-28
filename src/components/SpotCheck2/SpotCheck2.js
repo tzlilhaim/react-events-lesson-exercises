59722b6b19155d4f73cfc434b4bd35257eb98cce
Hunter Weinsheink <hunteraweinsheink@gmail.com>
2019-04-28 12:08:26 +0300

    fixed test instructions

--- src/components/SpotCheck2/SpotCheck2.js
+++ src/components/SpotCheck2/SpotCheck2.js
@@ -12,7 +12,8 @@ class SpotCheck2 extends Component {
 
   // Your SpotCheck2 should render the Home Component, the About Component, and a Toggle Button
   // Either the Home or About component should be rendered when you press the toggle button
-  // You must use state and setState
+  // You should have a state with the property "showHome", with a value of true or false
+  // You must use state and setState, to change the value of showHome
 
   render() {
     return (
